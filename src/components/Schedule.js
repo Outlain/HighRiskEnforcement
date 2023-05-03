ea16cfbc5c626708a9abcdf7b75871db45690598
Carlos Amado <exchangeside@gmail.com>
2023-05-03 00:32:36 -0400

    fixed bug

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -115,7 +115,7 @@ export function Schedule() {
 
                                                         <div className="hour-value">
                                                             {console.log(hours[0][selectedDay])}
-                                                            {hours[0][selectedDay].D <= 1 || hours[0][selectedDay].G <= 1 ? (
+                                                            {hours[0][selectedDay].D >= 1 || hours[0][selectedDay].G >= 1 ? (
                                                                 <>
                                                                     <span className='d'>{hours[0][selectedDay].D}</span>
                                                                     <span className='g'>{hours[0][selectedDay].G}</span>
